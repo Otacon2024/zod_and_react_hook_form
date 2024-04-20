4d557f1e23afbe5d8832ece4bb0626b1de44aee6
Guillaume Climent <79173984+guillaumecliment@users.noreply.github.com>
2024-04-20 23:58:06 +0200

    minor change

--- src/zod-test01.ts
+++ src/zod-test01.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 const UserSchema = z.object({
   username: z.string(),
@@ -10,7 +10,7 @@ const UserSchema = z.object({
 
 type User = z.infer<typeof UserSchema>;
 
-const correctUser: User = { username: 'John Doe' };
+const correctUser: User = { username: "John Doe" };
 const wrongUser: User = { username: 1 };
 
 // console.log(UserSchema.parse(correctUser));
@@ -28,10 +28,9 @@ console.log(UserSchema.safeParse(correctUser)); // Should work
 console.log(UserSchema.safeParse(wrongUser)); // Should return an error object and let the program continue
 
 // 2. this solution works too + provide more readability and "more flexibility to handle it in various ways, making the code easier to maintain and extend. For example, you might want to add additional logic that only runs when parsing is successful."
-const result = UserSchema.safeParse(correctUser;
+const result = UserSchema.safeParse(correctUser);
 if (result.success) {
-  console.log('Parsed data:', result.data);
+  console.log("Parsed data:", result.data);
 } else {
-  console.error('Validation failed:', result.error);
+  console.error("Validation failed:", result.error);
 }
-
